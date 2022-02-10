8328c9dc6b0c17f943220fe0af2b1ec03782e8b7
André Quintino <pandrequintino@gmail.com>
2022-02-10 23:36:44 +0000

    feat: novos icones adicionados a familia de icones

---
+++ components/icon-family/icon-options.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+
+import { IconProps } from '../../types';
+
+function IconOptions({ ...props }: IconProps) {
+	return (
+		<svg
+			width={props.size || '24'}
+			height={props.size || '24'}
+			viewBox="0 0 24 24"
+			fill="none"
+			xmlns="http://www.w3.org/2000/svg"
+		>
+			<path
+				d="M6.645 15L12 20.5674L17.355 15L19 16.7224L12 24L5 16.7224L6.645 15Z"
+				fill={props.color || 'gray'}
+			/>
+			<path
+				d="M17.355 9L12 3.43261L6.645 9L5 7.27763L12 0L19 7.27763L17.355 9Z"
+				fill={props.color || 'gray'}
+			/>
+		</svg>
+	);
+}
+
+export { IconOptions };
--- components/icon/index.tsx
+++ components/icon/index.tsx
@@ -1,10 +1,17 @@
 import React from 'react';
 
 import {
+	IconBookmark,
 	IconDelivery,
+	IconDocument,
+	IconFeed,
 	IconFire,
+	IconHome,
+	IconMail,
 	IconMap,
+	IconOptions,
 	IconRestaurant,
+	IconSettings,
 	IconSilverware,
 	IconWallet,
 } from '../icon-family';
@@ -17,14 +24,28 @@ type IconBaseProps = {
 
 function IconBase({ ...props }: IconBaseProps): JSX.Element {
 	switch (props.iconName) {
+		case 'bookmark':
+			return <IconBookmark size={props.size} color={props.color} />;
 		case 'delivery':
 			return <IconDelivery size={props.size} color={props.color} />;
+		case 'document':
+			return <IconDocument size={props.size} color={props.color} />;
+		case 'feed':
+			return <IconFeed size={props.size} color={props.color} />;
 		case 'fire':
 			return <IconFire size={props.size} color={props.color} />;
+		case 'home':
+			return <IconHome size={props.size} color={props.color} />;
+		case 'mail':
+			return <IconMail size={props.size} color={props.color} />;
 		case 'map':
 			return <IconMap size={props.size} color={props.color} />;
+		case 'options':
+			return <IconOptions size={props.size} color={props.color} />;
 		case 'restaurant':
 			return <IconRestaurant size={props.size} color={props.color} />;
+		case 'settings':
+			return <IconSettings size={props.size} color={props.color} />;
 		case 'silverware':
 			return <IconSilverware size={props.size} color={props.color} />;
 		case 'wallet':
