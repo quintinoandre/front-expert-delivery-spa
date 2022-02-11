4f53b3c8306ddad3816776038f1a1b2daefcaa11
André Quintino <pandrequintino@gmail.com>
2022-02-11 08:28:35 +0000

    feat: atualizado icones do sidebar

--- components/sidebar/index.tsx
+++ components/sidebar/index.tsx
@@ -1,6 +1,7 @@
 import Link from 'next/link';
 import React from 'react';
 
+import { IconBase } from '../icon';
 import Styles from './styles.module.css';
 
 function Sidebar() {
@@ -15,7 +16,9 @@ function Sidebar() {
 				<li>
 					<Link href="/">
 						<a>
-							<span>icon</span>
+							<span>
+								<IconBase iconName="home" color="#A3A3A4" />
+							</span>
 							Home
 						</a>
 					</Link>
@@ -23,7 +26,9 @@ function Sidebar() {
 				<li className={Styles.active}>
 					<Link href="/explore">
 						<a>
-							<span>icon</span>
+							<span>
+								<IconBase iconName="feed" color="#A3A3A4" />
+							</span>
 							Explore
 						</a>
 					</Link>
@@ -31,7 +36,9 @@ function Sidebar() {
 				<li>
 					<Link href="/favorites">
 						<a>
-							<span>icon</span>
+							<span>
+								<IconBase iconName="bookmark" color="#A3A3A4" />
+							</span>
 							Favorites
 						</a>
 					</Link>
@@ -39,7 +46,9 @@ function Sidebar() {
 				<li>
 					<Link href="/orders">
 						<a>
-							<span>icon</span>
+							<span>
+								<IconBase iconName="document" color="#A3A3A4" />
+							</span>
 							Orders
 						</a>
 					</Link>
@@ -47,7 +56,9 @@ function Sidebar() {
 				<li>
 					<Link href="/messages">
 						<a>
-							<span>icon</span>
+							<span>
+								<IconBase iconName="mail" color="#A3A3A4" />
+							</span>
 							Messages
 						</a>
 					</Link>
@@ -55,7 +66,9 @@ function Sidebar() {
 				<li>
 					<Link href="/settings">
 						<a>
-							<span>icon</span>
+							<span>
+								<IconBase iconName="settings" color="#A3A3A4" />
+							</span>
 							Settings
 						</a>
 					</Link>
