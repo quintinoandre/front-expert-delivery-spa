6bf6281c42040e34fbaabc33363d2c46f29f87df
André Quintino <pandrequintino@gmail.com>
2022-02-15 19:28:12 +0000

    feat: adicionado novas categorias, cores e alinhamento

--- components/categories/index.tsx
+++ components/categories/index.tsx
@@ -8,18 +8,39 @@ function Categories() {
 	const categories = [
 		{
 			name: 'Popular',
-			legend: '+286 options',
+			legend: '286+ options',
 			iconName: 'fire',
+			backgroundSection: '#FB6D34',
 		},
 		{
 			name: 'Fast delivery',
 			legend: '1,843+ options',
-			iconName: 'bike',
+			iconName: 'delivery',
+			backgroundSection: '#503E9D',
 		},
 		{
 			name: 'High class',
 			legend: '25+ options',
 			iconName: 'wallet',
+			backgroundSection: '#FACD5D',
+		},
+		{
+			name: 'Dine in',
+			legend: '182+ options',
+			iconName: 'silverware',
+			backgroundSection: '#FB6D34',
+		},
+		{
+			name: 'Pick up',
+			legend: '3,348+ options',
+			iconName: 'home',
+			backgroundSection: '#503E9D',
+		},
+		{
+			name: 'Nearest',
+			legend: '44+ options',
+			iconName: 'map',
+			backgroundSection: '#FACD5D',
 		},
 	];
 	return (
@@ -30,7 +51,7 @@ function Categories() {
 					<a>See all</a>
 				</Link>
 			</div>
-			<div className="flex gap-6">
+			<div className="flex gap-6 justify-between">
 				{categories.map((category) => (
 					<Category key={category.name} {...category} />
 				))}
