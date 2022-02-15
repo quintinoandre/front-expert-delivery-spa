4d810598ec96b1a2c68c5567ebf83ec4e22d9542
André Quintino <pandrequintino@gmail.com>
2022-02-15 23:07:40 +0000

    feat: adicionado item componente restaurantes em destaque

---
+++ components/highlights-item/index.tsx
@@ -0,0 +1,57 @@
+import Image from 'next/image';
+import React from 'react';
+
+import {
+	IconStar,
+	IconMoney,
+	IconSilverware,
+	IconLocation,
+} from '../icon-family';
+import Styles from './styles.module.css';
+
+type Props = {
+	name: string;
+	rate: number;
+	comments: number;
+	food: string;
+	distance: number;
+	price: number;
+	bonus: string;
+	brandUrl: string;
+};
+
+function HighlightsItem({ ...props }: Props) {
+	return (
+		<div className={Styles.restaurant}>
+			<div className={Styles.brand}>
+				<Image src={props.brandUrl} alt={props.name} width={64} height={64} />
+			</div>
+			<div className={Styles.information}>
+				<h3>{props.name}</h3>
+				<div className="my-3">
+					<div>
+						<IconStar size={16} color="#FACD5D" />
+						<span>{props.rate}</span>({props.comments})
+					</div>
+					<div>
+						<IconSilverware size={16} color="#A3A3A4" />
+						{props.food}
+					</div>
+					<div>
+						<IconMoney size={16} color="#A3A3A4" />
+						{props.price}
+					</div>
+				</div>
+				<div>
+					<div>{props.bonus}</div>
+					<div>
+						<IconLocation size={16} color="#A3A3A4" />
+						{props.distance}
+					</div>
+				</div>
+			</div>
+		</div>
+	);
+}
+
+export { HighlightsItem };
