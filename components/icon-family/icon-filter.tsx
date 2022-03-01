ba1dfa583b6a3f78fb84ee2cc188355e29a426d5
andre <pandrequintino@gmail.com>
2022-03-01 00:56:16 +0000

    feat: adicionado novos icones

---
+++ components/icon-family/icon-filter.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+
+import { IconProps } from '../../types';
+
+function IconFilter({ ...props }: IconProps) {
+	return (
+		<svg
+			width={props.size || '20'}
+			height={props.size || '20'}
+			viewBox="0 0 20 20"
+			fill="none"
+			xmlns="http://www.w3.org/2000/svg"
+		>
+			<path
+				fillRule="evenodd"
+				clipRule="evenodd"
+				d="M0 3V5.33333H20V3H0ZM3.33333 11.1667H16.6667V8.83333H3.33333V11.1667ZM12.2222 17H7.77778V14.6667H12.2222V17Z"
+				fill={props.color || 'gray'}
+			/>
+		</svg>
+	);
+}
+
+export { IconFilter };
